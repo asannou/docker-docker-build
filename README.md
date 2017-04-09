f945b980b812635aa6783497f89c454946d9186e
asannou <hataori@gmail.com>
2017-04-09 21:35:02 +0900

    イメージ名変更

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ```
-$ docker run -it --rm -v /var/run/docker.sock:/docker.sock asannou/docker-run -it --rm -- asannou/docker-run-test arg
+$ docker run -it --rm -v /var/run/docker.sock:/docker.sock asannou/docker-build:run -it --rm -- asannou/docker-build-test arg
+```
+
+or
+
+```
+$ docker run -it --rm -v /var/run/docker.sock:/docker.sock asannou/docker-build asannou/docker-build-test
+$ docker run -it --rm asannou/docker-build/asannou/docker-build-test arg
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,38 @@
 #!/bin/sh
 set -e
 
-while [ "$1" != '--' ]
-do
-  [ -z "$1" ] && exit
-  OPTIONS="$OPTIONS $1"
+if [ $# -eq 1 ]
+then
+  IMAGE=$1
   shift
-done
-
-IMAGE=$2
-shift 2
-
-nginx -c /etc/nginx/proxy.conf
+else
+  while [ "$1" != '--' ]
+  do
+    [ -z "$1" ] && exit
+    OPTIONS="$OPTIONS $1"
+    shift
+  done
+  IMAGE=$2
+  shift 2
+fi
 
 DOCKER='docker -H unix:///docker.sock'
+
+SELF=$($DOCKER inspect --format='{{.Config.Image}}' $HOSTNAME)
+PREFIX=$(echo $SELF | cut -d : -f 1)
+TAG=$(echo $SELF | cut -d : -f 2)
+
+sed -i -e "s#{{image}}#$PREFIX/$IMAGE#" /etc/nginx/proxy.conf
+nginx -c /etc/nginx/proxy.conf
+
 [ -z "$($DOCKER images -q $IMAGE)" ] && $DOCKER pull $IMAGE
+
 WD=$($DOCKER inspect --format='{{.Config.WorkingDir}}' $IMAGE)
 [ -z "$WD" ] && WD=/root
+
 ID=$($DOCKER create -v $WD $IMAGE /bin/true)
 trap "$DOCKER rm $ID > /dev/null" INT EXIT
+
 $DOCKER run -a stderr --rm --link $HOSTNAME:host --volumes-from $ID -w $WD docker -H host build -t $IMAGE .
-$DOCKER run $OPTIONS 0_$IMAGE "$@"
+
+[ "$TAG" = 'run' ] && $DOCKER run $OPTIONS $PREFIX/$IMAGE "$@"
